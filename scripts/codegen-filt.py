eb1118f7aa1c9311fd5020ef148b03206b31cc77
Dmitri Nikulin <dnikulin@gmail.com>
2011-07-29 16:06:36 +1000

    Generate typical file header for filt.cl

--- scripts/codegen-filt.py
+++ scripts/codegen-filt.py
@@ -1,10 +1,29 @@
-OFFSETS = [
-    (x, y)
-    for y in [-1, 0, 1]
-    for x in [-1, 0, 1]
-]
+print """// Copyright (C) 2011  Dmitri Nikulin, Monash University
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
 
-print """
+// Enable OpenCL 32-bit integer atomic functions.
+#pragma OPENCL EXTENSION cl_khr_global_int32_base_atomics : enable
+
 kernel void fast_filter(
     read_only  image2d_t   scores,
     global     int2      * corners,
@@ -23,6 +42,12 @@ kernel void fast_filter(
     int  const p00 = read_imageui(scores, sampler, xy).x;
 
     // Read other scores in a tight square around the candidate score."""
+
+OFFSETS = [
+    (x, y)
+    for y in [-1, 0, 1]
+    for x in [-1, 0, 1]
+]
 
 for (shift, (x, y)) in enumerate(OFFSETS):
     print ("    int  const p%02d = read_imageui(scores, sampler, xy + (int2)(%2d, %2d)).x;" % (shift + 1, x, y))
