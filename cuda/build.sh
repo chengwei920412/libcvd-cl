fd27770b71b353b5c01b3a07dc16e798cf4785f0
Dmitri Nikulin <dnikulin@gmail.com>
2011-11-02 13:46:46 +1100

    Attempt to improve nvcc build flags

--- cuda/build.sh
+++ cuda/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 python cugen-fast1-gray.py > fast1_gen.cu
 python cugen-fast2-gray.py > fast2_gen.cu
-nvcc -DCVD_IMAGE_DEBUG --gpu-architecture=compute_20 --compiler-bindir=/usr/bin/gcc-4.4 -O3 -lcvd -o bin-test fast.cu
+nvcc -DCVD_IMAGE_DEBUG -arch=sm_23 --compiler-bindir=/usr/bin/gcc-4.4 -O3 -lcvd -o bin-test fast.cu
 ./bin-test
 
