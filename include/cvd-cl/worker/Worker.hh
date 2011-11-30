8dd16670a8940016adf5fb85c40674e0110c4eb1
Dmitri Nikulin <dnikulin@gmail.com>
2011-11-30 11:03:28 +1100

    Draft Doxygen for Worker.hh

--- include/cvd-cl/worker/Worker.hh
+++ include/cvd-cl/worker/Worker.hh
@@ -36,30 +36,76 @@
 namespace CVD {
 namespace CL  {
 
+/// A single OpenCL device that can maintain states and execute steps.
+/// \see WorkerState
+/// \see WorkerStep
 class Worker : public boost::noncopyable {
 public:
 
+    /// \brief Construct a worker for a given OpenCL device.
+    ///
+    /// A context and command queue are created automatically.
     explicit Worker(cl::Device & device);
+
+    /// \brief Release resources allocated for this worker.
     virtual ~Worker();
 
+    /// \brief Convenience method to compile an OpenCL program with a single kernel.
+    ///
+    /// Consider using embed.py to generate headers with string constants.
+    ///
+    /// \param program  OpenCL program object that will receive the compiled program (given by \a source and \a options).
+    /// \param kernel   OpenCL kernel object that will receive the program's kernel (given by \a name).
+    /// \param source   OpenCL program source code.
+    /// \param name     Kernel name as found in \a source.
+    /// \param options  OpenCL compiler options.
     void compile(cl::Program * program, cl::Kernel * kernel,
                  char const  * source,  char const * name,
                  char const  * options = "");
 
+    /// \brief Issue an OpenCL command queue barrier using \c clEnqueueBarrier.
     void barrier();
+
+    /// \brief Wait for the OpenCL command queue to finish using \c clFinish.
     void finish();
 
-    // Leave public for direct access by steps and states.
+
+    // --- Leave members public for direct access by steps and states ---
+
+
+    /// \brief OpenCL device used by this worker.
     cl::Device               device;
+
+    /// \brief Vector of OpenCL devices, containing only #device.
     std::vector<cl::Device>  devices;
+
+    /// \brief OpenCL context containing only #device.
     cl::Context              context;
+
+    /// \brief OpenCL command queue related to #device and #context.
     cl::CommandQueue         queue;
 
+    /// \brief Maximum local work group size for 1 dimension, as determined from \c clGetInfo.
     size_t      const        maxLocalSize;
+
+    /// \brief Default local work group size for 1 dimension, based on
+    /// #maxLocalSize and an implicit limit (currently 512) that is
+    /// necessary for unusual implementations.
     size_t      const        defaultLocalSize;
+
+    /// \brief #defaultLocalSize wrapped in a \c cl::NDRange.
     cl::NDRange const        defaultLocal;
 
+    /// \brief Round global size up to a multiple of #defaultLocalSize.
+    /// \param items  Number of items actually in the global work size.
+    /// \return An integer at least as large as \a items and a
+    ///         multiple of #defaultLocalSize.
     size_t                   padGlobalSize(size_t items);
+
+    /// \brief Round global size up to a multiple of #defaultLocalSize
+    /// and wrap in a \c cl::NDRange.
+    /// \return A \c cl::NDRange with 1 dimension at least as large as \a items and a
+    ///         multiple of #defaultLocalSize.
     cl::NDRange              padGlobal(size_t items);
 };
 
